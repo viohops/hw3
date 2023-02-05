d0cf465445236f8ed18244b8168e7f5c119a0ad9
unknown <vitalii.programm@gmail.com>
2023-02-05 12:37:23 +0300

    Вторая задача

--- Program.cs
+++ Program.cs
@@ -8,15 +8,38 @@
 
 // 23432 -> да
 
-System.Console.Write("Введите число: ");
-string a = Console.ReadLine();
-char[] arr = a.ToCharArray();
-Array.Reverse(arr);
-string b = new string(arr);
+// System.Console.Write("Введите число: ");
+// string a = Console.ReadLine();
+// char[] arr = a.ToCharArray();
+// Array.Reverse(arr);
+// string b = new string(arr);
 
-if(a == b){
-    System.Console.WriteLine("Число является палиндромом");
+// if(a == b){
+//     System.Console.WriteLine("Число является палиндромом");
+// }
+// else{
+//     System.Console.WriteLine("Не является палиндромом");
+// }
+
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+int InputNumber(string str)
+{
+    System.Console.WriteLine(str);
+    return Convert.ToInt32(Console.ReadLine());
 }
-else{
-    System.Console.WriteLine("Не является палиндромом");
-}
+
+int x1 = InputNumber("Введите координату Х1: ");
+int y1 = InputNumber("Введите координату Y1: ");
+int z1 = InputNumber("Введите координату Z1: ");
+
+int x2 = InputNumber("Введите координату Х2: ");
+int y2 = InputNumber("Введите координату Y2: ");
+int z2 = InputNumber("Введите координату Z2: ");
+
+
+System.Console.WriteLine(Math.Round(Math.Pow(Math.Pow((x2-x1),2) + Math.Pow((y2-y1),2) + Math.Pow((z2-z1),2),0.5),2));
